5dc66ebcdcd58b5182597d5e0ba105da30230023
gaozhen <3168175817@qq.com>
2018-04-14 21:39:54 +0800

    发送邮件
    Signed-off-by: gaozhen <3168175817@qq.com>

--- src/test/java/com/gz/springbootdemo/test/TestMail.java
+++ src/test/java/com/gz/springbootdemo/test/TestMail.java
@@ -1,7 +1,11 @@
 package com.gz.springbootdemo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
@@ -12,5 +16,17 @@ import com.gz.springbootdemo.Application;
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
 public class TestMail {
+	@Autowired
+	private JavaMailSender mailSender;
 
+	@Test
+	public void sendSimpleMail() throws Exception {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setFrom("gaozhen3h@163.com");
+		message.setTo("3168175817@qq.com");
+		message.setSubject("主题：简单邮件");
+		message.setText("测试邮件内容");
+
+		mailSender.send(message);
+	}
 }
